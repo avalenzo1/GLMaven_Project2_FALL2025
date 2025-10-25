eb3b3caa1dda06f0a1be7c6b061dd779483239a7
Anthony Valenzo <94491552+avalenzo1@users.noreply.github.com>
2025-10-25 18:25:07 -0500

    added drawings selection, spot selection.

--- src/main/java/GameScreen.java
+++ src/main/java/GameScreen.java
@@ -1,11 +1,18 @@
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GameScreen {
@@ -19,14 +26,17 @@ public class GameScreen {
     VBox resultsDisplay;
     NumberButton[][] numberButtons;
     Set<Integer> selectedNumbers;
-    int spotsCount;
-    int drawingsCount;
+
+    int numDrawings;
+    int numSpots;
     int currentDrawing;
     GameLogic gameLogic;
 
 
     GameScreen() {
         root = new VBox();
+        root.setStyle("-fx-background-color: #D4AF37;");
+
         controlPanel = new HBox();
         controlPanel.setAlignment(Pos.CENTER);
         betCard = new GridPane();
@@ -34,38 +44,88 @@ public class GameScreen {
         gameLogic = new GameLogic();
 
         resultsDisplay = new VBox();
-        spotsCount  = 0;
-        drawingsCount = 0;
-        currentDrawing = 0;
 
         setupMenu();
         setupDrawingsSelection();
         setupSpotsSelection();
 
-
+        root.getChildren().add(drawingsSelection);
     }
 
     Scene createGameScene() {
-        return new Scene(root);
+        return new Scene(root,500, 400);
     }
 
     void setupMenu() {
         menuBar = new MenuBar();
+
     }
 
     void initializeBetCard() {
         numberButtons = new NumberButton[8][10];
 
+
+        root.getChildren().clear();
+
     }
 
     void setupSpotsSelection() {
         spotsSelection = new VBox();
+        spotsSelection.setAlignment(Pos.CENTER);
+        spotsSelection.setPadding(new Insets(5));
+        spotsSelection.setSpacing(5);
+
+        Text spotsText = new Text("How Many Spots Do You Want To Play?");
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(5);
+
+        int[] spots = { 1, 4, 8, 10 };
+
+        for (int i = 0; i < 4; i++) {
+            int spot = spots[i];
+
+            Button spotButton = new Button();
+            spotButton.setText(String.valueOf(spot));
+            spotButton.setOnAction(e -> {
+                numSpots = spot;
+                initializeBetCard();
+            });
+
+            hBox.getChildren().add(spotButton);
+        }
+
+        spotsSelection.getChildren().addAll(spotsText, hBox);
     }
 
     void setupDrawingsSelection() {
         drawingsSelection = new VBox();
-        drawingsSelection.setStyle("-fx-background-color: #D4AF37;");
-        drawingsSelection.getChildren().addAll(menuBar);
+        drawingsSelection.setAlignment(Pos.CENTER);
+        drawingsSelection.setPadding(new Insets(5));
+        drawingsSelection.setSpacing(5);
+
+        Text drawingsText = new Text("How Many Drawings Do You Want To Pick?");
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(5);
+
+        for (int i = 0; i < 4; i++) {
+            int drawing = i + 1;
+
+            Button drawingBtn = new Button();
+            drawingBtn.setText(String.valueOf(drawing));
+            drawingBtn.setOnAction(e -> {
+                numDrawings = drawing;
+                root.getChildren().clear();
+                root.getChildren().add(spotsSelection);
+            });
+
+            hBox.getChildren().add(drawingBtn);
+        }
+
+        drawingsSelection.getChildren().addAll(drawingsText, hBox);
 
     }
 
@@ -78,7 +138,7 @@ public class GameScreen {
     }
 
     void startDrawings() {
-        root.getChildren().add(drawingsSelection);
+
 
     }
 
